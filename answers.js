b31b4cbbaf76eae5930da78faefd0bf5a9455613
acenjo <72635314+acenjo@users.noreply.github.com>
2021-02-19 12:05:44 -0500

    Add files via upload

---
+++ answers.js
@@ -0,0 +1,14 @@
+//1. Java Script
+//2. HTML
+//3. A division tag can be used to seperate parts and rename them.
+   //A division tag can also be used to group stuff and optimize the group.
+//4. The origins/starting points are different.
+//5. Opacity is used fo the transparency of objects.
+//6. The programming language is called jsx.
+//7. The editor we're using is snack.expo
+//8. First, you log into your account on another device. 
+   //Then, you open up the code and run it on iOS.
+   //Finally, you press Tap to Play and type in the number on your device.
+//9. The render function is used to print out the code.
+//10. The return function gives us omething in return when you put something in.
+//1. We aded the red button component, the views component, and the text component.
